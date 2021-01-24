a31ab06e2d4d7699d67e44a0838e5770fd836680
yzR1991 <49443945+yzR1991@users.noreply.github.com>
2021-01-24 19:29:45 +0800

    Update Coral.py

--- code/deep/DDC_DeepCoral/Coral.py
+++ code/deep/DDC_DeepCoral/Coral.py
@@ -2,9 +2,11 @@ import torch
 DEVICE = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
 def CORAL(source, target):
+    #获取tensor的行列信息
     d = source.size(1)
     ns, nt = source.size(0), target.size(0)
 
+    #covariance计算可自行推导，或参考Unsupervised Domain Adaptation by Mapped Correlation Alignment
     # source covariance
     tmp_s = torch.ones((1, ns)).to(DEVICE) @ source
     cs = (source.t() @ source - (tmp_s.t() @ tmp_s) / ns) / (ns - 1)
